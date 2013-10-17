dc0e465d5b694424fa120c33fc820f86419ee7ab
sshaw <skye.shaw@gmail.com>
2013-10-17 01:31:57 -0400

    more template specs

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "tempfile"
 
 describe JAXB2Ruby::Template do
   it "uses the default template if none is given" do
@@ -9,10 +10,28 @@ describe JAXB2Ruby::Template do
     end
   end
 
+  it "uses the template provided as an argument" do
+    file = Tempfile.new("jaxb2ruby")
+    file.write("DATA")
+    file.close
+
+    t = JAXB2Ruby::Template.new(file.path)
+    t.build(nil).must_equal("DATA")
+  end
+
   it "raises an error if the template can't be read" do
     lambda { JAXB2Ruby::Template.new("blah!") }.must_raise(JAXB2Ruby::Error, /cannot load/)
   end
 
-  it "passes the given class to the template"
-  it "builds a list of the available templates" 
+  it "passes the given class to the template" do
+    file = Tempfile.new("jaxb2ruby")
+    file.write("<%= @class.name %>")
+    file.close
+
+    klass = Struct.new(:name).new("sshaw")
+    t = JAXB2Ruby::Template.new(file.path)
+    t.build(klass).must_equal("sshaw")
+  end
+
+  it "builds a list of the available templates"
 end
