2539be7871e97e1dbc0f83d90106ef7bf19b62c9
sshaw <skye.shaw@gmail.com>
2013-10-27 23:23:58 -0400

    add specs for ruby template

--- lib/jaxb2ruby.rb
+++ lib/jaxb2ruby.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/object/blank"
 require "active_support/core_ext/string"
 
+require "jaxb2ruby/version"
 require "jaxb2ruby/classes"
 require "jaxb2ruby/converter"
 require "jaxb2ruby/template"
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,5 +1,11 @@
 require "spec_helper"
+require "fileutils"
+require "tmpdir"
 require "tempfile"
+
+def write(path, classdef)
+  File.open(path, "w") { |io| io.puts(classdef) }
+end
 
 describe JAXB2Ruby::Template do
   it "uses the default template if none is given" do
@@ -34,4 +40,79 @@ describe JAXB2Ruby::Template do
   end
 
   it "builds a list of the available templates"
+
+  describe "a class created by the ruby template" do
+    let(:tmpdir)     { Dir.mktmpdir }
+    let(:attributes) {
+      Hash[
+        :user_name  => "sshaw",
+        :first_name => "skye",
+        :last_name  => "shaw",
+        :numbers    => [1, 2, 3],
+        :age        => 0xFF
+      ]
+    }
+
+    before do
+      path = File.join(tmpdir, "user.rb")
+      classes = convert("class")
+
+      t = JAXB2Ruby::Template.new("ruby")
+      write(path, t.build(classes.first))
+
+      require path
+    end
+
+    after { FileUtils.rm_rf(tmpdir) }
+
+    it "has singular read/write accessors for bounded elements" do
+      methods = Com::Example::User.instance_methods(false)
+      methods.must_include(:user_name)
+      methods.must_include(:user_name=)
+      methods.must_include(:first_name)
+      methods.must_include(:first_name=)
+      methods.must_include(:last_name)
+      methods.must_include(:last_name=)
+    end
+
+    it "has plural read/write accessors for unbounded elements" do
+      methods = Com::Example::User.instance_methods(false)
+      methods.must_include(:numbers)
+      methods.must_include(:numbers=)
+    end
+
+    it "has singular read/write accessors for attributes" do
+      methods = Com::Example::User.instance_methods(false)
+      methods.must_include(:age)
+      methods.must_include(:age=)
+    end
+
+    describe ".new" do
+      it "accepts no arguments" do
+        user = Com::Example::User.new
+        user.must_be_instance_of(Com::Example::User)
+        attributes.each { |name, _| user.send(name).must_be_nil }
+      end
+
+      it "accepts a nil argument" do
+        user = Com::Example::User.new(nil)
+        user.must_be_instance_of(Com::Example::User)
+        attributes.each { |name, _| user.send(name).must_be_nil }
+      end
+
+      it "initializes instances using values in the Hash argument" do
+        user = Com::Example::User.new(attributes)
+        user.must_be_instance_of(Com::Example::User)
+        attributes.each { |name, value| user.send(name).must_equal(value) }
+      end
+
+      # Or, should an ArgumentError be raised?
+      it "ignores unknown accessors names in the Hash argument"
+    end
+
+    describe "#inspect" do
+      it "returns a description of the class' state" do
+      end
+    end
+  end
 end
